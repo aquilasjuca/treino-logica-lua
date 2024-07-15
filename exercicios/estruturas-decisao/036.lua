cad06efd5b9ac187582c542116641d6272141f6f
Áquilas Rodrigues Jucá <aquilasjuca2016@gmail.com>
2024-07-15 19:25:10 -0300

    036

--- exercicios/estruturas-decisao/036.lua
+++ exercicios/estruturas-decisao/036.lua
@@ -22,7 +22,7 @@ if ano <= 0 then
       print("Dia Inválido!")
     end
   elseif mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8
-    or mes == 10 or mes == 10 then
+    or mes == 10 or mes == 12 then
     if dia > 0 and dia <= 31 then
       print("Data Válida!")
     else
